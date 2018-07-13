bacb2aad4f001774f15594b985f425ed22cc9be3
proepkes <pb156875@fh-muenster.de>
2018-07-13 14:54:51 +0200

    Version Updates

--- Assets/SpeedDate/Scripts/Spawner.cs
+++ Assets/SpeedDate/Scripts/Spawner.cs
@@ -8,53 +8,53 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-	private static Spawner _instance;
-	
-	private readonly SpeedDateClient _client = new SpeedDateClient();
-	
-	public string MasterIpAddress = IPAddress.Loopback.ToString();
-	public int MasterPort = 60125;
-	public string PathToGameServerExecutable = @"C:\gameserver.exe";
-	public string MachineIpAddress = IPAddress.Loopback.ToString();
-	
-	private void Awake()
-	{
-		if (_instance == null)
-		{
-			_instance = this;
-			DontDestroyOnLoad(this);
-		}
-		else
-		{
-			Destroy(gameObject);
-		}
-	}
-	
-	// Use this for initialization
-	void Start ()
-	{
-		_client.Started += () =>
-		{
-			Debug.Log("Spawner connected to server"); 
-			
-			GetPlugin<SpawnerPlugin>().RegisterSpawner(new SpawnerOptions(), spawner =>
-			{
-				Debug.Log("Spawner registered to server");
-			}, Debug.Log);
-		};
-		_client.Start(new DefaultConfigProvider(new NetworkConfig(MasterIpAddress, MasterPort), PluginsConfig.DefaultSpawnerPlugins, new IConfig[]
-		{
-			new SpawnerConfig
-			{
-				SpawnInBatchmode = true,
-				ExecutablePath = PathToGameServerExecutable,
-				MachineIp = MachineIpAddress
-			}, 
-		}));
-	}
-	
-	public T GetPlugin<T>() where T : class, IPlugin
-	{
-		return _client.GetPlugin<T>();
-	}
+    private static Spawner _instance;
+
+    private readonly SpeedDateClient _client = new SpeedDateClient();
+    public string MachineIpAddress = IPAddress.Loopback.ToString();
+    public string PathToGameServerExecutable = @"C:\gameserver.exe";
+
+    public string MasterIpAddress = IPAddress.Loopback.ToString();
+    public int MasterPort = 60125;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        _client.Started += () =>
+        {
+            Debug.Log("Spawner connected to server");
+
+            GetPlugin<SpawnerPlugin>().Register(spawner => { Debug.Log("Spawner registered to server"); }, Debug.Log);
+        };
+        _client.Start(new DefaultConfigProvider(
+            new NetworkConfig(MasterIpAddress, MasterPort),
+            PluginsConfig.DefaultSpawnerPlugins, 
+            new IConfig[]
+            {
+                new SpawnerConfig
+                {
+                    SpawnInBatchmode = true,
+                    ExecutablePath = PathToGameServerExecutable,
+                    MachineIp = MachineIpAddress
+                }
+            }));
+    }
+
+    public T GetPlugin<T>() where T : class, IPlugin
+    {
+        return _client.GetPlugin<T>();
+    }
 }
